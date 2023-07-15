c620797e03f9f695a3a9df1882d2a88a91432a3b
Shryansh <shryansh2024@gmail.com>
2023-07-15 23:39:39 +0530

    changed index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,6 @@
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors");
 app.use(cors());
